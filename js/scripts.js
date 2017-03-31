81e3c15ecda8b3144d806fd7a51d30a897026308
Trevor Fast <trevorrfast@gmail.com>
2017-03-31 13:42:22 -0700

    add complete frontend javascript to be tested when backend completed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 
  // backend
+ function products()
+    if() {
 
+    } else if {
+
+    } else if {
+
+    } else if {
+
+    } else {
+
+    }
 
 
  // frontend
  $(document).ready(function(){
    $("#form").submit(function(event){
      event.preventDefault();
-     var input = $("number-input").val();
-     if((input)){
-       
-     }
+     var pingpong = $("number-input").val();
+     var printscreen = products(pinpong);
+      printscreen.forEach(function(element){
+        $("#output").append("<li><h2>" + element + "</h2></li>");
+      });
 
+
+     }
+     return
    });
 
 });
