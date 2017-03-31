6e8fac248be0b7b3802cbe90056188aa4af87298
Trevor Fast <trevorrfast@gmail.com>
2017-03-31 11:52:15 -0700

    add partial front end JavaScript logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-$(function(){
+
  // backend
 
 
 
  // frontend
+ $(document).ready(function(){
+   $("#form").submit(function(event){
+     event.preventDefault();
+     var input = $("number-input").val();
+     if((input)){
+       
+     }
+
+   });
 
 });
