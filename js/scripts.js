7cdefc55e4a2bef3ed4b7e0fa14342b87d029771
Trevor Fast <trevorrfast@gmail.com>
2017-03-31 15:55:35 -0700

    fix the mispelling of the parameter on line six of the .js document.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-// frontend
+            // frontend
 $(document).ready(function(){
-  $("#form").submit(function(event){
+  $("#form").click(function(event){
     event.preventdefault();
     var pingpong = $("number-input").val();
-    console.log("1");
-    var printscreen = products(pinpong);
+    var printscreen = products(pingpong);
     printscreen.foreach(function(element){
       $("#output").append("<li><h2>" + element + "</h2></li>");
     });
@@ -13,12 +12,11 @@ $(document).ready(function(){
 
 
 
- // backend
+            // backend
  function products(pingpong){
    var numbers = [];
    for (var i = 1; i <= pingpong; i ++) {
     if (i % 3) {
-      console.log("2");
       numbers.push("ping!");
   } else if (i % 5) {
       numbers.push("pong!");
