f7a31484cc4c3eaac6b24d52e4554b9d17476b22
Trevor Fast <trevorrfast@gmail.com>
2017-03-31 15:04:57 -0700

    finish the if else if for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
+// frontend
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventdefault();
+    var pingpong = $("number-input").val();
+    var printscreen = products(pinpong);
+    printscreen.foreach(function(element){
+      $("#output").append("<li><h2>" + element + "</h2></li>");
+    });
+
+  });
+});
+
+
 
  // backend
  function products(pingpong){
    var numbers = [];
-   for(var i = 1; i <= pingpong; i ++) {
-    if(3 % i) {
-      push.("ping!")
-
-
-    }
+   for (var i = 1; i <= pingpong; i ++) {
+     console.log("hello");
+    if (i % 3) {
+      numbers.push("ping!");
+  } else if (i % 5) {
+      numbers.push("pong!");
+  } else if ( i % 15) {
+      numbers.push("PINGPONG!");
+  }  else {
+      numbers.push(i);
   }
+
+
 }
-    return
-
- // frontend
- $(document).ready(function(){
-   $("#form").submit(function(event){
-     event.preventDefault();
-     var pingpong = $("number-input").val();
-     var printscreen = products(pinpong);
-      printscreen.forEach(function(element){
-        $("#output").append("<li><h2>" + element + "</h2></li>");
-      });
-
-   });
-});
+  return numbers;
+}
